3ce8f307b7e2cb98ab17017cc97772a3b2d9f950
Manish Manohar <beingmanish.manohar@gmail.com>
2025-05-29 15:00:52 +0530

    Optimize `twoSum` with hash map approach and add TypeScript configuration

--- problems/two-sum/two-sum.ts
+++ problems/two-sum/two-sum.ts
@@ -1,8 +1,10 @@
 function twoSum(nums: number[], target: number): number[] {
+    const map = new Map<number, number>();
     for (let i = 0; i < nums.length; i++) {
-        const num = target - nums[i];
-        const index = nums.indexOf(num, i + 1);
-        if (index !== -1) return [i, index];
+        const complement = target - nums[i];
+        if (map.has(complement))
+            return [map.get(complement), i];
+        map.set(nums[i], i);
     }
     return [];
 }
